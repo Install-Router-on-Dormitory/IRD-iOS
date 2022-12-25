f3dac22c92569399198b8819e82b73dca63a3df4
baegteun <baegteun@gmail.com>
2022-12-26 08:42:31 +0900

    :sparkles: :: NFCReader

--- IRD/Sources/Scenes/Home/HomeView.swift
+++ IRD/Sources/Scenes/Home/HomeView.swift
@@ -1,15 +1,56 @@
-//
-//  HomeView.swift
-//  IRD
-//
-//  Created by 최형우 on 2022/12/24.
-//
-
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack(spacing: 16) {
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .clipShape(Circle())
+
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("2118")
+                        .font(.headline)
+
+                    Text("변찬우")
+                }
+
+                Spacer()
+            }
+            .padding(4)
+            .background {
+                Color.white
+                    .cornerRadius(12)
+            }
+            .shadow(radius: 6)
+
+            Spacer()
+
+            Button {
+                
+            } label: {
+                VStack {
+                    HStack {
+                        Image(systemName: "iphone.homebutton.radiowaves.left.and.right")
+
+                        Text("누르지마시오")
+                    }
+                    .foregroundColor(.red)
+                }
+                .padding(12)
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color(hex: "ffffff"))
+                        .clipShape(Capsule())
+                        .shadow(color: .black.opacity(0.4), radius: 4)
+                }
+                .padding(.top, 16)
+            }
+
+            Spacer()
+        }
+        .padding(16)
     }
 }
 
